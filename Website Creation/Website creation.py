12ce3a24d2201edadf68913f80cdfb0bbbe977ba
kamga <kamganganthony@gmail.com>
2023-03-29 04:53:09 +0200

    Added website with trailers

--- Website Creation/Website creation.py
+++ Website Creation/Website creation.py
@@ -1,4 +1,5 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
@@ -11,4 +12,29 @@ avatar = media.Movie("Avatar",
                      "https://m.media-amazon.com/images/I/51vtp5pgmnL._AC_.jpg",
                      "https://www.youtube.com/watch?v=-egQ79OrYCs&ab_channel=hollywoodstreams")
 
-print(avatar.stor)
+pirates = media.Movie("Pirates of the caribbean",
+                      "At World's End",
+                      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQlOw-O1tdmzrkCgS5iezwNZ3-4iaRQKdCpVU1tQW-9yzGqJOk8",
+                      "https://www.youtube.com/watch?v=rF6k1bXAVIg&ab_channel=%5B%E2%96%BA%5DLBA-LaBande-Annonce",)
+
+harry_potter = media.Movie("Harry Potter",
+                           "The Sorcerer's Stone",
+                           "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTYGx6w4wW7xmC8h_UdhyzyxhOV4QZofI0lrgZ7JgMkCyqGG5M_",
+                           "https://www.youtube.com/watch?v=VyHV0BRtdxo&ab_channel=RottenTomatoesClassicTrailers")
+
+facebook = media.Movie("The Social Network",
+                       "Facebook",
+                       "http://www.movienewsletters.net/photos/086829R1.jpg",
+                       "https://www.youtube.com/watch?v=lB95KLmpLR4&ab_channel=SonyPicturesEntertainment")
+
+social_dilemma = media.Movie("The social dilemma",
+                             "We tweet, we like, and we share",
+                             "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSlr2S2rTPojczGtrzbf14XwnH-O9ze4FypnKUT3AGPyTgp2X8t",
+                             "https://www.youtube.com/watch?v=uaaC57tcci0&ab_channel=Netflix")
+
+movies = [toy_story, avatar, facebook, social_dilemma, pirates, harry_potter]
+fresh_tomatoes.open_movies_page(movies)
+
+#avatar.show_trailer()
+#pirates.show_trailer()
+
